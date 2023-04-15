92599aedb1cdd2982967d9be3136d94f49d4f2ee
rheisan <rheisanfrs24@gmail.com>
2023-04-15 14:10:25 +0700

    create latihan 1.php

---
+++ Modul 8/Latihan 1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Latihan 1</title>
+    </head>
+    <body>
+        <!-- tag <?php ?> untuk kode php dalam html -->
+        <?php
+            echo 'Kode PHP di sini';
+        ?>
+        <p>Dokumen HTML</p>
+        <?php
+            echo 'Kode PHP di sini';
+        ?>
+        
+        <!-- menyingkat tag php dalam satu baris -->
+        <p>Kode <?php echo "PHP";?> di HTML</p>
+    </body>
+</html>
