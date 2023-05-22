6cd9a22c6e66d10c653729819a3f73aa065bd0a4
rheisan <rheisanfrs24@gmail.com>
2023-05-22 22:41:32 +0700

    create db_prakweb.php

---
+++ Modul 11/Latihan/db_prakweb.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="id">
+<?php
+    include("koneksi.php");
+?>
+    <head>
+        <title>Tes Koneksi</title>
+    </head>
+    <body>
+        <h4>Koneksi Berhasil</h4>
+    </body>
+</html>
